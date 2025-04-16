6783d9a1fad5b6ecb53e4d15a3465f833d256589
Jery <jery99961@gmail.com>
2025-04-16 11:50:42 +0530

    add: gh-pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	base: "/StudyPal_AI",	// base URL for gh-pages
 	resolve: {
 		alias: {
 			"@components": path.resolve(__dirname, "./src/components"),
